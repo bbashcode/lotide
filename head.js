d24728131810119744511c88faf6b2fccc1b5576
bbashcode <binbash.code@gmail.com>
2022-01-11 23:58:37 +0000

    adding tail function to get the tail of a givern array. Added edge cases for head.js. Adding assertEqual for testing.

--- head.js
+++ head.js
@@ -9,6 +9,8 @@ const assertEqual = function(actual, expected) {
 const head = function(arr) {
   if (arr.length > 0) {
     return arr[0];
+  } else {
+    return undefined;
   }
 };
 
---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+const tail = function(arr) {
+  let tailA = [];
+  if (arr.length > 0) {
+    tailA = arr.splice(1);
+  } else {
+    return tailA;
+  }
+  return tailA;
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
