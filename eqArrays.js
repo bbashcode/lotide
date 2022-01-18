49def64062284d4a88c443e5e612b62e3494d2a0
bbashcode <binbash.code@gmail.com>
2022-01-18 06:05:32 +0000

    implemented eqArray function to compare between two arrays to check if they are equal

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+const eqArrays = function (arr1, arr2){
+
+  for(let num1 of arr1){
+    for(num2 of arr2) {
+      if(num1 === num2){
+        return true;
+      }
+      return false;
+    }
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [4, 2, 7]), true); // => should FAIL
