6bcc4cb6288391b045e8170a2aa6622908bcfda1
bbashcode <binbash.code@gmail.com>
2022-01-25 17:32:13 +0000

    implemented counteLetters function in Lotide

---
+++ countLetter.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+
+const countLetters = function(sentence) {
+  let results = {};
+
+  for (let char of sentence) {
+    if (results[char] !== " ") {
+      if (results[char]) {
+        results[char] += 1;
+      } else {
+        results[char] = 1;
+      }
+    }
+      
+  }
+
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
